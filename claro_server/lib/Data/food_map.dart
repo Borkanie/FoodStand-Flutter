d52cd02b31e5350452b83f5799aef6b65cca7be0
borkanie <bogdanboboc1789@yahoo.com>
2024-10-20 20:38:07 +0300

    manual override of food_map to json.

--- claro_server/lib/Data/food_map.dart
+++ claro_server/lib/Data/food_map.dart
@@ -1,13 +1,9 @@
 import 'package:claro_server/Data/container.dart';
 import 'package:claro_server/Data/location.dart';
-import 'package:json_annotation/json_annotation.dart';
-
-part 'food_map.g.dart';
 
 /// This object holds a colleciton of [Container] in a matrix like format using a [Map]
 /// It set's a fixed [lines] and [columns] on construction to frame the matrix.
 /// The Map will use the [Location] of each container  as the key.
-@JsonSerializable()
 class FoodMap{
   Map<Location,Container> containers;
   final int lines;
@@ -37,8 +33,36 @@ class FoodMap{
     return other is FoodMap ? other.hashCode == hashCode : false;
   }
 
-  factory FoodMap.fromJson(Map<String, dynamic> json) => _$FoodMapFromJson(json);
-  Map<String, dynamic> toJson() => _$FoodMapToJson(this);
+  /// Manual `fromJson` method
+  factory FoodMap.fromJson(Map<String, dynamic> json) {
+    Map<Location, Container> containerMap = {};
+
+    for (var entry in json['containers']) {
+      var location = Location.fromJson(entry['location']);  // Assuming Location has a fromJson method
+      var container = Container.fromJson(entry['container']);
+      containerMap[location] = container;
+    }
+
+    return FoodMap(
+      containerMap,
+      json['lines'] as int,
+      json['columns'] as int,
+    );
+  }
+  
+ /// Manual `toJson` method
+  Map<String, dynamic> toJson() {
+    return {
+      'containers': containers.entries.map((entry) {
+        return {
+          'location': entry.key.toJson(),  // Assuming Location has a toJson method
+          'container': entry.value.toJson()
+        };
+      }).toList(),
+      'lines': lines,
+      'columns': columns,
+    };
+  }
 }
 
 
